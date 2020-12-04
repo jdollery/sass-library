e047f3af8cf61fd0ef246d35e1fbcfcd64d9ab66
jdollery <54100876+jdollery@users.noreply.github.com>
2020-12-04 09:46:55 +0000

    Work on drying up grid

---
+++ dist/index.php
@@ -0,0 +1,283 @@
+<?php
+
+$posts = array(
+
+  array(
+    "title"   => "CCPA and GDPR: An Overview and Comparison of the Laws",
+    "date" => "10 July 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => true
+  ), 
+  
+  array(
+    "title"   => "EDPB guidelines: cookies, consent and compliance",
+    "date" => "25 June 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ), 
+  
+  array(
+    "title"   => "Google Tag Manager and Automatic Cookie Blocking",
+    "date" => "5 May 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ),
+
+  array(
+    "title"   => "CCPA and GDPR: An Overview and Comparison of the Laws",
+    "date" => "10 July 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ), 
+  
+  array(
+    "title"   => "EDPB guidelines: cookies, consent and compliance",
+    "date" => "25 June 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => true
+  ), 
+  
+  array(
+    "title"   => "Google Tag Manager and Automatic Cookie Blocking",
+    "date" => "5 May 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ),
+
+  array(
+    "title"   => "CCPA and GDPR: An Overview and Comparison of the Laws",
+    "date" => "10 July 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ),
+
+  array(
+    "title"   => "Google Tag Manager and Automatic Cookie Blocking",
+    "date" => "5 May 2020",
+    "img" => "https://via.placeholder.com/1080x650",
+    "content" => "Phasellus sagittis ullamcorper libero a euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+    "url" => "/news/article",
+    "filled" => false
+  ),
+  
+);
+
+?>
+<!--[if lt IE 7]><html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7" lang="en-GB"> <![endif]-->
+<!--[if IE 7]><html class="no-js ie ie7 lt-ie9 lt-ie8" lang="en-GB"> <![endif]-->
+<!--[if IE 8]><html class="no-js ie ie8 lt-ie9" lang="en-GB"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<!DOCTYPE html>
+<!--<![endif]-->
+<html lang="en">
+  <head>
+    <meta name="robots" content="noindex" />
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles.css">
+  </head>
+  <body>
+    <header class="header">Header</header>
+    <main>
+
+      <article>
+        <div class="content">
+          <div class="content__row">
+            <div class="inner">
+
+              <h1>Header 1</h1>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h2>Header 2</h2>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h3>Header 3</h3>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h4>Header 4</h4>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h5>Header 5</h5>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <img src="https://via.placeholder.com/1920x1080">
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h6>Header 6</h6>
+              <p>Lorem ipsum dolor sit amet, <a href="/">consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+              <p>Consectetur adipiscing elit</a>. In non maximus nibh. Aenean interdum faucibus nunc a ultricies. Integer ornare sapien quis nunc accumsan scelerisque. Morbi eu turpis id nibh sagittis efficitur.</p>
+
+              <h4>Header 4</h4>
+              <ul>
+                <li>Consectetur adipiscing elit</li>
+                <li>Posuere cubilia curae</li>
+              </ul>
+
+              <ol>
+                <li>Consectetur adipiscing elit</li>
+                <li>Posuere cubilia curae</li>
+              </ol>
+
+              <h3>Header 3</h3>
+              <div class="table-responsive">
+                <table>
+                  <caption>A sample table</caption>
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th id="head1" scope="col">Test head</th>
+                      <th id="head2" scope="col">Test head</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row" data-title="#">1</th>
+                      <td headers="head1" data-title="Test head">Test content</td>
+                      <td headers="head2" data-title="Test head">Test content</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" data-title="#">2</th>
+                      <td headers="head1" data-title="Test head">Test content</td>
+                      <td headers="head2" data-title="Test head">Test content</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" data-title="#">3</th>
+                      <td headers="head1" data-title="Test head">Test content</td>
+                      <td headers="head2" data-title="Test head">Test content</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              <p>
+                <a href="https://csslayout.io/patterns" target="_blank">https://csslayout.io/patterns</a><br>
+                <a href="https://frontendor.com/preview/" target="_blank">https://frontendor.com/preview/</a><br>
+                <a href="https://luna.sainsburys.co.uk/guidelines" target="_blank">https://luna.sainsburys.co.uk/guidelines</a><br>
+                <a href="https://pulse.heartbeat.ua/" target="_blank">https://pulse.heartbeat.ua/</a>
+              </p>
+
+              <button class="button--outlined" type="button">Button</button>
+              <a href="/" class="button--outlined" role="button"><span class="icon"><svg role="img"><use xlink:href="#iconLink" href="#iconLink"></use></svg></span> Link Button</a>
+              <button class="button--outlined" type="button" disabled>Button Disabled</button>
+              <a href="/" class="button--outlined button--disabled" role="button" aria-disabled="true" tabindex="-1">Link Button Disabled</a>
+
+              <button class="button--filled" type="button">Button</button>
+              <a href="/" class="button--filled" role="button"><span class="icon"><svg role="img"><use xlink:href="#iconLink" href="#iconLink"></use></svg></span> Link Button</a>
+              <button class="button--filled" type="button" disabled>Button Disabled</button>
+              <a href="/" class="button--filled button--disabled" role="button" aria-disabled="true" tabindex="-1">Link Button Disabled</a>
+
+              <button class="button--danger" type="button">Button</button>
+              <a href="/" class="button--danger" role="button"><span class="icon"><svg role="img"><use xlink:href="#iconLink" href="#iconLink"></use></svg></span> Link Button</a>
+              <button class="button--danger" type="button" disabled>Button Disabled</button>
+              <a href="/" class="button--danger button--disabled" role="button" aria-disabled="true" tabindex="-1">Link Button Disabled</a>
+
+              <button class="button--success" type="button">Button</button>
+              <a href="/" class="button--success" role="button"><span class="icon"><svg role="img"><use xlink:href="#iconLink" href="#iconLink"></use></svg></span> Link Button</a>
+              <button class="button--success" type="button" disabled>Button Disabled</button>
+              <a href="/" class="button--success button--disabled" role="button" aria-disabled="true" tabindex="-1">Link Button Disabled</a>
+
+              <form class="form">
+                <fieldset>
+                  <div class="form__input">
+                    <label for="inputOne">Input cell</label>
+                    <input
+                      type="text"
+                      id="inputOne"
+                      name="inputOne"
+                      placeholder="Input placeholder"
+                      required
+                      aria-describedby="inputOneHelp"
+                    />
+                    <small id="inputOneHelp" class="form__input__help">We'll never share your email with anyone else.</small>
+                  </div>
+                  <div class="form__input">
+                    <label for="inputReadonly">Input Read only</label>
+                    <input
+                      type="text"
+                      id="inputReadonly"
+                      name="inputReadonly"
+                      placeholder="Read only"
+                      readonly
+                    />
+                  </div>
+                  <div class="form__input">
+                    <label for="inputDisabled">Input Disabled</label>
+                    <input
+                      type="text"
+                      id="inputDisabled"
+                      name="inputDisabled"
+                      placeholder="Disabled"
+                      disabled
+                    />
+                  </div>
+                  <div class="form__input">
+                    <label for="inputOne">Textarea</label>
+                    <textarea id="inputTextarea" rows="3"></textarea>
+                  </div>
+                  <input
+                      type="submit"
+                      id="inputSubmit"
+                      class="button--filled"
+                      name="inputSubmit"
+                      value="Submit"
+                    />
+                </fieldset>
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <section>
+        <div class="content">
+          <div class="content__row">
+            <div class="inner">
+              <div class="inner__row">
+                <?php foreach($posts as $post) { ?>
+                <article class="post">
+                  <h4><?php echo $post['title'] ?></h4>
+                  <p><?php echo $post['content'] ?></p>
+                  <button class="button--outlined" type="button">Learn more</button>
+                </article>
+                <?php } ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="iconLink" width="100%" height="100%" viewBox="0 0 439.4 439.4" preserveAspectRatio="none" >
+          <path d="M259.2,6.2L116.1,149.1c-4.2,4.2-6.4,9.8-6.4,15.4c0,5.6,2.1,11.1,6.3,15.4c8.5,8.5,22.2,8.5,30.7,0L252.6,74.2v217.1
+            c0,57.6-46.9,104.7-104.5,104.9H21.7c-12,0-21.7,9.7-21.7,21.7c0,12,9.7,21.7,21.7,21.7h126.5c81.6-0.3,147.9-66.8,147.9-148.3
+            V73.7l106.2,106.1c8.5,8.5,22.3,8.5,30.7,0c8.5-8.5,8.5-22.2,0-30.7L289.9,6.2C281.5-2.3,267.7-2.3,259.2,6.2z"/>
+        </symbol>
+      </svg>
+
+    </main>
+  </body>
+</html>
