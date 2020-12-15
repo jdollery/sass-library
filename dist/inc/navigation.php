ade11a9275b8dc36cc35ba79a794c85358008fd5
jdollery <54100876+jdollery@users.noreply.github.com>
2020-12-15 17:59:16 +0000

    Work on global head styling

---
+++ dist/inc/navigation.php
@@ -0,0 +1,23 @@
+<div class="navigation" id="headerNav" >
+  <h4 id="primaryNavigation" class="sr-only">Primary navigation</h4>
+  <ul aria-labelledby="primaryNavigation">
+    <li><a href="#" title="Home">Home</a></li>
+    <li><a href="#" title="About">About</a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
--- dist/index.php
+++ dist/index.php
@@ -104,35 +104,14 @@ $posts = array(
               Brand Logo
             </a>
           </div>
-          <div class="header__center">
-            <div class="navigation" id="headerNav" >
-              <h4 id="primaryNavigation" class="sr-only">Primary navigation</h4>
-              <ul aria-labelledby="primaryNavigation">
-                <li><a href="#" title="Home">Home</a></li>
-                <li><a href="#" title="About">About</a>
-                  <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a>
-                      <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Contact</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="#">Contact</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </div>
-          </div>
+          <!-- <div class="header__center">
+            
+          </div> -->
           <div class="header__right">
             <button class="burger" id="navToggle" type="button" title="Main menu">
               <div class="burger__inner"></div>
             </button>
+            <?php include("inc/navigation.php"); ?>
           </div>
         </div>
       </div>
